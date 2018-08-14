b3a40d27d5a3039a19649fc3ecd569657bcbe56c
Jill <globaldetroithuman@gmail.com>
2018-08-14 12:24:05 -0400

    update git for slack help

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './RoomList';
+
+//src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCT4ly9M2fNLRYwvQDRTxEUOgljc0opGT0",
+    authDomain: "bloc-chat-5a1ba.firebaseapp.com",
+    databaseURL: "https://bloc-chat-5a1ba.firebaseio.com",
+    projectId: "bloc-chat-5a1ba",
+    storageBucket: "bloc-chat-5a1ba.appspot.com",
+    messagingSenderId: "19757901664"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -13,9 +28,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <RoomList 
+        rooms={this.props.room}
+      />
       </div>
     );
   }
 }
+
+
 
 export default App;
